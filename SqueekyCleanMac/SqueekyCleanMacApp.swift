c623c4998c02bd39c2dcf82163d7215b43fce94c
Fabien Brocklesby <fabienbrocklesby@icloud.com>
2026-05-14 14:04:24 +1200

    Initial Commit

---
+++ SqueekyCleanMac/SqueekyCleanMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  SqueekyCleanMacApp.swift
+//  SqueekyCleanMac
+//
+//  Created by Fabien Brocklesby on 14/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct SqueekyCleanMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
